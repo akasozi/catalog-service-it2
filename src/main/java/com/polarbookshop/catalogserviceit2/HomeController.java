ec93fee0cc14506fb6f30f2d61bbe65634a70a7c
abukasozi <abukasozi@gmail.com>
2024-11-02 12:34:48 +0300

    Added getString route

--- src/main/java/com/polarbookshop/catalogserviceit2/HomeController.java
+++ src/main/java/com/polarbookshop/catalogserviceit2/HomeController.java
@@ -1,2 +1,13 @@
-package com.polarbookshop.catalogserviceit2;public class HomeController {
+package com.polarbookshop.catalogserviceit2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String getString() {
+        return "Welcome to the book catalog!";
+    }
 }
