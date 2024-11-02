928d9f0ed863e3574143b81e077c4dfb1a318b6c
abukasozi <abukasozi@gmail.com>
2024-11-02 12:07:52 +0300

    Initial Commit

---
+++ src/main/java/com/polarbookshop/catalogserviceit2/CatalogServiceIt2Application.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.catalogserviceit2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceIt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogServiceIt2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/polarbookshop/catalogserviceit2/CatalogServiceIt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.catalogserviceit2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceIt2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
